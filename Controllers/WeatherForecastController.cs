36fec0a163c82c27907f97f1797cb90f0a001b3e
csuarez .ice <csuarez.ice@ibm.com>
2024-01-03 23:59:04 -0500

    Se implementa ef core database firts y se rea repositorio basado en abstracciones con el patron de diseño repository con generics

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+using RepositoryPatternDesign;
+using RepositoryPatternDesign.Models;
+
+namespace ApiRestN5.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IRepository<TipoPermiso> _repository;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IRepository<TipoPermiso> repository)
+        {
+            _logger = logger;
+            _repository = repository;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            _repository.Add(new TipoPermiso { Descripcion="Permiso por enfermedad" });
+            _repository.Save();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
